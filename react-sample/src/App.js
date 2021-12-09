181a9c3af432bdd7d27a7db9cd5af39447180c78
Isaac J <36749051+IsaacJrTypes@users.noreply.github.com>
2021-12-09 15:07:04 -0800

    add display text changes

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          IT115 group 1 Demo
         </p>
         <a
           className="App-link"
